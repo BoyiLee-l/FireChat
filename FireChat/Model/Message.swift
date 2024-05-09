e4a5990d648e9120eae872f496f012e1b668ad21
duncan <p396520@gmail.com>
2024-05-09 14:31:16 +0800

    新增聊天室介面ui

---
+++ FireChat/Model/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  FireChat
+//
+//  Created by Duncan Li on 2024/5/9.
+//
+
+import Foundation
+
+struct Message {
+    let text: String
+    let isFromCurrentUser: Bool
+}
